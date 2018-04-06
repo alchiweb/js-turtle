8ff7c8a0ef95f90d96f01d48b701b8fa3493f3b1
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-04-06 13:44:25 +0000

    color argument options added

--- turtle.js
+++ turtle.js
@@ -9,12 +9,27 @@ pendown() 	    //իջեցնել մատիտը
 reset() 	                 //սկսել սկզբից, ջնջել ամեն ինչ ....
 angle(angle)	   //անկյուն	 
 width(width)       //մատիտի հաստությունը(հաստություն)
-color(r,g,b) 	   //ընտրել գույն(), արգումենտը կամ երեք թիվ, կամ զանգված 
+
+//ընտրել գույն(), արգումենտը, երեք թիվ, զանգված, գույնի անունը կամ #հեքսկոդը
+color(r,g,b) 	   
 color([r,g,b])
+color("red")
+color("#ff0000")
 */
 
 width(5);
-
 forward(50);
 left(90);
+color("red");
 forward(100);
+forward(50);
+left(90);
+color("#00ff00");
+forward(100);
+forward(50);
+left(90);
+color(255,255,0);
+forward(100);
+color([100,50,120]);
+left(80);
+forward(155);
